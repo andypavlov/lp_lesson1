24a9af9e1c0ff85b520379331de9312a3735dd99
Andrey Pavlov <yokinda@gmail.com>
2018-12-01 16:14:04 +0300

    Применил format

--- price.py
+++ price.py
@@ -1,5 +1,5 @@
 def format_price(price):
 	price = int(price)
-	return 'Цена: ' + str(price) + ' руб.'
+	return 'Цена: {0} руб.'.format(price)
 display_price = format_price(56.24)
 print(display_price)
